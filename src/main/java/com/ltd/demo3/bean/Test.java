10e82189f35ea65445b210464868e1bb9c211f57
Xavier <228753175@qq.com>
2018-08-10 00:18:04 +0800

    Update new bean of【test】

---
+++ src/main/java/com/ltd/demo3/bean/Test.java
@@ -0,0 +1,21 @@
+/**
+ * FileName: Test
+ * Author:   Xavier
+ * Date:     2018/8/10 0:10
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号          描述
+ */
+package com.ltd.demo3.bean;
+
+/**
+ * 〈〉
+ *
+ * @author Xavier
+ * @create 2018/8/10
+ * @since 1.0.0
+ */
+public class Test {
+
+}
