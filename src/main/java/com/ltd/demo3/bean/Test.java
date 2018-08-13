10b3e67237652a71a282f391016cb04896e230eb
Xavier <228753175@qq.com>
2018-08-14 00:08:02 +0800

    Update new bean of【test】 11111

--- src/main/java/com/ltd/demo3/bean/Test.java
+++ src/main/java/com/ltd/demo3/bean/Test.java
@@ -18,4 +18,13 @@ package com.ltd.demo3.bean;
  */
 public class Test {
 
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
